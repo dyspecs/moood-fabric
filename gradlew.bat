bc90f9c20bc94ec76a61de6fe662bbed98f57e52
dyspecs <dyspecs@gmail.com>
2025-08-10 20:50:22 -0300

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,18 @@
-@ECHO OFF
-ECHO Descargando Gradle Wrapper...
-gradlew.bat %*
+@echo off
+rem -----------------------------------------------------------------------------
+rem Gradle start up script for Windows
+rem -----------------------------------------------------------------------------
+
+set DIRNAME=%~dp0
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set GRADLE_WRAPPER_JAR=%APP_HOME%gradle\wrapper\gradle-wrapper.jar
+set GRADLE_WRAPPER_PROPERTIES=%APP_HOME%gradle\wrapper\gradle-wrapper.properties
+
+if not exist "%GRADLE_WRAPPER_JAR%" (
+    echo ERROR: Gradle wrapper jar not found.
+    exit /b 1
+)
+
+java -jar "%GRADLE_WRAPPER_JAR%" %*
